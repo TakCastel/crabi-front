501d6cb5ae18350ea41381085b0b4ca7e9a03413
TakCastel <takcastel@gmail.com>
2019-07-03 15:09:23 +0200

    🔖 Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app :dark="dark">
+    <v-toolbar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      fixed
+      class="px-2"
+      app
+    >
+      <span>TakCastel &copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'Roleplay',
+    dark: true
+  })
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+    <v-flex xs12>
+      <div>
+        Hello and welcome to my roleplay app !
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
