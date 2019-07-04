db6ddd2e9c2f2abbf4072b49736c344bbf622a21
TakCastel <takcastel@gmail.com>
2019-07-04 16:53:47 +0200

    🔋NuxtServerInit request cookie session

--- store/index.js
+++ store/index.js
@@ -1,9 +1,26 @@
+const cookieparser = process.server ? require('cookieparser') : undefined
+
 export const state = () => ({
 
 })
 
 export const actions = {
-
+  nuxtServerInit({ commit }, { req }) {
+    let session = {
+      user: 'Anonymous',
+      jwt: undefined
+    }
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        session = JSON.parse(parsed.auth)
+        commit('auth/AUTHENTICATE_USER')
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+    commit('auth/RESTORE_SESSION', session)
+  }
 }
 
 export const mutations = {
