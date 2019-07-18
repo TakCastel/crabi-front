e0bc4db5f32fcb4a5787d68d67e7b48181481f93
TakCastel <takcastel@gmail.com>
2019-07-18 17:33:42 +0200

    🌿Answer button atomic component

--- components/atoms/CButtonAnswer/index.vue
+++ components/atoms/CButtonAnswer/index.vue
@@ -1,5 +1,49 @@
 <template>
-  <v-btn class="primary">
-    Répondre
-  </v-btn>
+  <div>
+    <v-btn color="primary" @click="dialog=true">
+      Répondre
+    </v-btn>
+    <v-dialog v-model="dialog" :fullscreen="$vuetify.breakpoint.xsOnly" max-width="800">
+      <v-card>
+        <v-toolbar color="primary">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Réponse</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <c-form-message variant="answer" @submission="dialog=false" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import CFormMessage from '@/components/molecules/CFormMessage'
+
+export default {
+  components: {
+    CFormMessage
+  },
+
+  data: () => ({
+    dialog: false
+  }),
+
+  computed: {
+    ...mapState({
+      current: state => state.threads.current
+    }),
+
+    defaultValue: function () {
+      const formValues = {
+        title: this.current.title,
+        message: this.current.body
+      }
+      return formValues
+    }
+  }
+}
+</script>
